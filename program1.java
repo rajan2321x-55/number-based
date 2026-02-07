3ecc9c04e68be528c763a7b85bdc2eec91e668be
rajan2321x-55 <rajan2321x@gmail.com>
2026-02-07 10:22:58 +0530

    Fix formatting and spacing in Program1

--- program1.java
+++ program1.java
@@ -3,16 +3,16 @@ package Number_based;
 public class Program1 {
 
 	public static void main(String[] args) {
-int num = 1234; // Sample Input
-        
+int num = 1234;
+      
         if (num == 0) {
             System.out.print(0);
         }
 
         while (num > 0) {
-            int digit = num % 10;      
+            int digit = num % 10;    
             System.out.print(digit + " "); 
-            num = num / 10;       
+            num = num / 10;      
         }
 
 	}
