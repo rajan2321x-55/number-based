0bd461bfeb5987116b8c81f0d852bdf45784c8c3
rajan2321x-55 <rajan2321x@gmail.com>
2026-02-07 10:23:55 +0530

    Fix count increment in digit counting logic

--- program2.java
+++ program2.java
@@ -6,13 +6,12 @@ public class Program2 {
 		int num = 4567; 
         int count = 0;
 
-
         if (num == 0) {
             count = 1;
         } else {
             while (num > 0) {
                 num = num / 10; 
-                count++;        
+                count++;    
             }
         }
 
